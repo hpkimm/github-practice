e0af78f125b7336495ab8b628b03bd0e21841c2d
hppp135@gmail.com <48650380+hpppppppp@users.noreply.github.com>
2022-02-22 21:42:39 +0900

    second commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,3 +1,16 @@
 const name =  prompt("이름을 입력해주세요");
 
 alert("반갑습니다 ${name}님, 환영합니다.");
+
+
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+const name =  prompt("이름을 입력해주세요");
+
